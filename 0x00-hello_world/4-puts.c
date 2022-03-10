a86ad1ae83573dc6b9dfb398c0e8db952c7f40ae
Mfon <92741946+themfon@users.noreply.github.com>
2022-03-11 00:33:00 +0100

    put c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,19 @@
-programming language is like building a multilingual puzzle
+/*
+ * File: 4-puts.c
+ * Auth: Mfon Etuk  
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints "Programming is like building a multilingual 
+ * 		puzzle."
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
