b556f82208f6ef96dc9db95fe51b6a600192c2ad
Mfon <92741946+themfon@users.noreply.github.com>
2022-03-11 19:46:52 +0100

    print numz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,25 @@
-Mfon Etuk
+/*
+ * File: 6-print_numberz.c
+ * Auth: Mfon Etuk
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0,
+ * 	only using putchar and without char variable.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num;
+
+	for (num =0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
+
+}
