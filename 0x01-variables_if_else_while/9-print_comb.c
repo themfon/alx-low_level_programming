220463a6a3ee0ce82a1659696a8cf7d98e312dc3
Mfon <92741946+themfon@users.noreply.github.com>
2022-03-11 20:02:50 +0100

    combination

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,1 +1,31 @@
-Mfon
+/*
+ * File: 9-print_comb.c
+ * Auth: Mfon Etuk
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints  all possible combinations od sindle-digit numbers
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+
+
