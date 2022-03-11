f25e610aac7c47413e1e7f1e7d8bdcd286279da5
Mfon <92741946+themfon@users.noreply.github.com>
2022-03-11 19:25:22 +0100

    print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,24 @@
-Mfon Etuk
+/*
+ * File: 2-print_alphabet.c
+ * Auth: Mfon Etuk
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: ALways 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+
+}
