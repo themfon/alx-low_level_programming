f27a820274f681187079c06e6c8dccfc3c0ae12d
Mfon <92741946+themfon@users.noreply.github.com>
2022-03-12 05:41:28 +0100

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,41 +1,32 @@
-/*
- * File: 1-last_digit.c
- * Auth: Mfon Etuk
- */
-
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 
 /**
- * main - Prints the last digit of a randomly generated number
- * 	and whether it is greater than 5, less than 6, or 0.
+ * main - Prints a text according number
  *
- * Return: Always 0,
- */
+ * Return: Always (Success)
+ **/
 int main(void)
 {
-	int n;
+		int n, lastd;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+		srand(time(0));
+		n = rand() - RAND_MAX / 2;
+		lastd = n % 10;
 
-	if ((n % 10) > 5)
-	{
-		printf("Last digit of %d is %d and is greater than 5\n",
-				n, n % 10);
-	}
-	else if ((n % 10) > 6 && (n % 10) != 0)
-	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n",
-				n, n % 10);
-	}
-	else
-	{
-		printf("Last digit of %d is %d and is 0\n",
-				n, n % 10);
-	}
+		if (lastd > 5)
+		{
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastd);
+		}
+		else if (lastd == 0)
+		{
+		printf("Last digit of %d is %d and is 0\n", n, lastd);
+		}
+		else if (lastd < 6 && lastd != 0)
+		{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
+		}
 
-	return (0);
-
+		return (0);
 }
