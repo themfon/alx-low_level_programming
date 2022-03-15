2c6b7b53f69db02b02c94992d0c3d5c8a52989d0
Mfon <92741946+themfon@users.noreply.github.com>
2022-03-15 19:16:06 +0100

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,19 @@
-alphabet
+/*
+ * File: 1-alphabet.c
+ * Auth: Precious Bassey
+ */
+
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase, followed by a new line.
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
