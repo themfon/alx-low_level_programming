4e1c79b713f390c7f4a198fbde729aa2c970a1af
Mfon <92741946+themfon@users.noreply.github.com>
2022-03-15 18:25:56 +0100

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,19 @@
-putchar
+/*
+ * File: 0-putchar.c
+ * Auth: Mfon Etuk
+ *
+ */
+
+#include "main.h"
+
+/**
+ * main - Prints "_putchar.c" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	write("_putchar\n");
+
+	return (0);
+}
