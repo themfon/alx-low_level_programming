d3207a702cbe51f57d8a92b7f12fa787ef5608c7
Mfon <92741946+themfon@users.noreply.github.com>
2022-03-21 17:46:48 +0100

    swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,1 +1,18 @@
-main
+#include "main.h"
+
+/**
+ * swap_int - swaping 2 integer using a pointer
+ * @a:integer
+ * @b:integer
+ * Return:void
+ */
+void swap_int(int *a, int *b)
+{
+	int x;
+
+	x = *a;
+	*a = *b;
+	*b = x;
+	return;
+
+}
