054ba46c01bbb6635091966e5a08ae196376aa43
Mfon <92741946+themfon@users.noreply.github.com>
2022-03-30 19:03:19 +0100

    square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -22,7 +22,7 @@ int power_operation(int n, int c)
  * @n: input number.
  * Return: natural square root.
  */
-int _sqrt_recursion(int n)
+int power_operation(int n)
 {
 	if (n < 0)
 		return (-1);
