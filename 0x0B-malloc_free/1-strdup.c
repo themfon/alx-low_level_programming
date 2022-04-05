77e7d6dbc0a189df19c2f4bf3b20898636ddaafb
Mfon <92741946+themfon@users.noreply.github.com>
2022-04-05 15:09:57 +0100

    pointer to allocated memory

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,11 +10,10 @@ char *_strdup(char *str)
 {
 	char *strout;
 	unsigned int i, j;
+	
 	if (str == NULL)
 		return (NULL);
-	
 	for (i = 0; str[i] != '\0'; i++);
-	
 	strout = (char *)malloc(sizeof(char) * (i + 1));
 	if (strout == NULL)
 		return (NULL);
