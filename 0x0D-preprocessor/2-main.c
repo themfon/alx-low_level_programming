09f482a3b61bb6c716d449a2962a05dc7b72e373
Mfon <92741946+themfon@users.noreply.github.com>
2022-04-11 17:15:36 +0100

    compilation sampled

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,1 +1,20 @@
-a
+#ifndef foo_H
+#define foo_H
+#include <stdio.h>
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+
+int main(void)
+{
+char name_storage[] = "2-main.c";
+
+printf("%s\n", __FILE__);
+
+return (0);
+}
+
+
+#endif
