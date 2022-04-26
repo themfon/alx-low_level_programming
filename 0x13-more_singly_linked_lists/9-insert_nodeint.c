635a456f3e470207a7f9fb95ca01245a21955e79
Mfon <92741946+themfon@users.noreply.github.com>
2022-04-26 04:37:53 +0100

    insert node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,4 +1,4 @@
-#include "lists.h"
+include "lists.h"
 
 /**
  * insert_nodeint_at_index - inserts a new node at a given position.
@@ -27,3 +27,21 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		(*head) = new;
 		return (new);
 	}
+
+	current = *head;
+	while (idx != 1)
+	{
+		current = current->next;
+		--idx;
+		if (current == NULL)
+		{
+			free(new);
+			return (NULL);
+		}
+	}
+	new->next = current->next;
+	current->next = new;
+
+	return (new);
+
+}
